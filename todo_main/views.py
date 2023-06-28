38560be57bf68c4915d8c94e3a4f45f1c8b25041
Piotrek <piotrkluziok@gmail.com>
2023-06-28 09:40:03 +0200

    Created todo model

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,7 +1,9 @@
 
 
 from django.shortcuts import render
-
+from todo.models import Task
 
 def home(request):
-    return render(request, 'home.html')
+    
+    tasks = Task.objects.filter(is_completed=False)
+    return render(request, 'home.html', {"tasks": tasks})
